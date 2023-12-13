be40ad99afee5b18461703956743efdd59cdf2bc
KhairuzzamanCP <129537822+KhairuzzamanCP@users.noreply.github.com>
2023-12-13 12:32:17 +0600

    music w4-pd2

---
+++ musicians_directory/album/forms.py
@@ -0,0 +1,10 @@
+from django import forms 
+from album.models import Album
+
+class AlbumForms(forms.ModelForm):
+    class Meta:
+        model = Album
+        fields = '__all__'
+        widgets = {
+            'release_date': forms.DateInput(attrs={'type': 'date'})
+        }
---
+++ musicians_directory/musician/forms.py
@@ -0,0 +1,13 @@
+from django import forms 
+from musician.models import Musicians
+
+class MusiciansForm(forms.ModelForm):
+    class Meta:
+        model = Musicians
+        fields = '__all__'
+
+        
+
+    
+
+        
